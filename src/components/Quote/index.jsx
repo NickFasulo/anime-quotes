e7290ac4f4a04c88bc8a37cdf94befa3a762867d
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-01-17 16:02:12 -0500

    finish quote css

--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -4,7 +4,7 @@ export default function Quote({ data }) {
   return (
     <div class='quote'>
       <div class='quote-content'>
-        <span>X</span>
+        <span>✕</span>
         <p>{data}</p>
       </div>
     </div>
